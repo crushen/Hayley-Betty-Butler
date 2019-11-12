b43cb523c5a5587c91b12e18c8674dd3a72e99e6
Charlotte <charlotterushen@hotmail.com>
2019-11-12 12:59:03 +0000

    Added mobile and desktop backgrounds

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>hello</h1>
+  <div id="home">
+    <div class="background-container" :style="{backgroundImage: `url(${backgroundMobile})`}">
+
+    </div>
+  </div>
 </template>
+
+<script>
+import backgroundMobile from '../assets/background-mobile.png';
+
+export default {
+  data() {
+    return {
+      backgroundMobile
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+  .background-container {
+   
+    width: 100%;
+    height: 100vh;
+    background-size: 126%;
+    background-repeat: no-repeat;
+  }
+  
+</style>
