38c53dc4730c4dbda4acd0e2966f114cc886cb18
Charlotte <charlotterushen@hotmail.com>
2019-11-12 14:46:24 +0000

    Changed font

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Muli', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
