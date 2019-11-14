4efba87243f84e5741a48334b813bbf919c0fed1
Charlotte Rushen <charlotterushen@hotmail.com>
2019-11-14 11:43:46 +0000

    font weight

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="bio">
-    <div class="content">
+    <div class="content section-padding">
       <p>A South London artist, singer/songwriter. Working with a variety of mediums to create pieces of work that explore emotion, colour, texture and light. Her art, words and music interrogate what it is to be human.</p>
     </div>
   </section>
@@ -13,5 +13,9 @@ export default {
 </script>
 
 <style scoped>
+  
 
+  p {
+    font-weight: 600;
+  }
 </style>
