14feca3a1014d3f5cb7944f9ee29ef8751157fc5
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-10 10:06:17 +0000

    Added solo pieces

---
+++ src/components/solo/SoloPieces.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="solo-pieces" class="section">
+    <div class="content">
+
+      <div class="title">
+        <h1>Solo Pieces</h1>
+      </div>
+
+      <div class="solo-piece" v-for="piece in soloPieces" :key="piece.id">
+        <div class="line"></div>
+        <img :src="piece.image.url" alt="">
+        <p class="img-title">{{ piece.title }}</p>
+        <p class="img-description">{{ piece.description }}</p>
+        <button>Enquire</button>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+const soloPieces = gql`
+  query {
+    soloPieces {
+      title,
+      description
+      image {
+        url
+      }
+    }
+  }
+`
+
+export default {
+  apollo: {
+    soloPieces: {
+      query: soloPieces
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <landing/>
     <collections/>
+    <solo-pieces/>
   </div>
 </template>
 
 <script>
 import Landing from '@/components/Landing.vue';
 import Collections from '@/components/collections/Collections.vue';
+import SoloPieces from '@/components/solo/SoloPieces.vue';
 
 export default {
   name: 'home',
   components: {
     Landing,
-    Collections
+    Collections,
+    SoloPieces
   }
 }
 </script>
